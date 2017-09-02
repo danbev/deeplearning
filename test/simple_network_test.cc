2754d2581457e8087b4fe59bc030e804ce462e0b
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-09-02 15:16:52 +0200

    test: rename result to prediction

--- test/simple_network_test.cc
+++ test/simple_network_test.cc
@@ -1,9 +1,9 @@
 #include "gtest/gtest.h"
 #include "neural-network.h"
 
-TEST(SimpleNetwork, simple) {
+TEST(SimpleNetwork, simple_network) {
   double input = 8.5;
   double weight = 0.1;
-  double result = simple_network(input, weight);
-  EXPECT_DOUBLE_EQ(result, 0.85);
+  double prediction = simple_network(input, weight);
+  EXPECT_DOUBLE_EQ(prediction, 0.85);
 }
